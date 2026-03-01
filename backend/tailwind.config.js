283775adcccf80619987f8e536aebdd65fe21d01
ScherbAlex <alexshcherbyna1173@gmail.com>
2026-03-01 14:05:19 +0300

    project_01_03

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -2,8 +2,20 @@
 module.exports = {
   content: [
     "./templates/**/*.html",
-    "./apps/**/templates/**/*.html"
+    "./apps/**/templates/**/*.html",
+    "./static/**/*.js"
   ],
-  theme: { extend: {} },
+  safelist: [
+    "bg-green-100",
+    "bg-green-200",
+    "bg-red-100",
+    "bg-red-200",
+    "bg-blue-500",
+    "ring-blue-100",
+    "border-blue-500",
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
 };
