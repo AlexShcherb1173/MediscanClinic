0ac6dab1e4140f79a4f443f91143c32e6b826b88
ScherbAlex <alexshcherbyna1173@gmail.com>
2026-03-03 22:01:39 +0300

    project_03_03

--- lint_format.bat
+++ lint_format.bat
@@ -1,51 +1,35 @@
 @echo off
-setlocal enabledelayedexpansion
-
-REM --- Переходим в backend (рядом с manage.py), независимо от места запуска ---
-cd /d "%~dp0backend" || (echo [ERROR] Cannot cd to backend & exit /b 1)
-
-REM --- Что проверяем ---
-set TARGETS=apps config
-
-for %%D in (%TARGETS%) do (
-  if not exist "%%D" (
-    echo [ERROR] Folder "%%D" not found in: %CD%
-    dir
-    exit /b 1
-  )
-)
+setlocal EnableExtensions
 
 echo ==================================================
 echo MediscanClinic: lint ^& format
-echo Workdir: %CD%
-echo Targets: %TARGETS%
 echo ==================================================
 
-echo.
+REM Переходим в backend (рядом с manage.py)
+cd /d "%~dp0backend" || (echo Cannot cd to backend & exit /b 1)
+
+set TARGETS=apps config
+
+echo Workdir: %CD%
+echo Targets: %TARGETS%
+echo --------------------------------------------------
+
 echo [1/4] Running black...
-python -m black %TARGETS% --extend-exclude "migrations"
+python -m black %TARGETS%
 if errorlevel 1 exit /b 1
 
-echo.
 echo [2/4] Running isort...
-python -m isort %TARGETS% --skip migrations
+python -m isort %TARGETS%
 if errorlevel 1 exit /b 1
 
-echo.
 echo [3/4] Running flake8...
-python -m flake8 %TARGETS% --config "%CD%\.flake8" --exclude=migrations
+python -m flake8 %TARGETS% --config .flake8
 if errorlevel 1 exit /b 1
 
-echo.
 echo [4/4] Running mypy...
-REM ВАЖНО: лучше настроить mypy в pyproject.toml / mypy.ini и не городить exclude в батнике
 python -m mypy %TARGETS%
 if errorlevel 1 exit /b 1
 
-echo.
-echo ✅ Done.
-
-REM Пауза только если запущено в интерактивной консоли
-if "%CI%"=="" pause
-
+echo Done.
+pause
 endlocal
